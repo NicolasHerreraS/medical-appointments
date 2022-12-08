ee2813d7dfa6139b2fd2de7977af20a296618b0a
Nicolás Herrera Sepúlveda <nicolas.herreras@autonoma.edu.co>
2022-12-07 23:01:59 -0500

    seguimiendo paso a paso 3 y 4

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,56 @@
 import React, {useState} from 'react';
-import {Text, Modal, Button, View} from 'react-native';
+import {Text, Modal, TextInput, Button, View} from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
-export const Form = ({modalVisible, newUser}) => {|
+export const Form = ({modalVisible, newUser}) => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [dateP, setDateP] = useState(new Date());
   return (
     <Modal animationType="slide" visible={modalVisible}>
       <Text>Formulario de registro</Text>
+      <Text style={styles.text_label}>Nombre</Text>
+      <TextInput
+        style={styles.text_input}
+        placeholder="Nombre"
+        value={user}
+        onChangeText={setUser}
+        multiline={true}
+        numberOfLines={5}
+      />
+      <Text style={styles.text_label}>Correo</Text>
+      <TextInput
+        style={styles.text_input}
+        placeholder="Correo"
+        value={email}
+        onChangeText={setEmail}
+        multiline={true}
+        numberOfLines={5}
+      />
+      <Text style={styles.text_label}>Teléfono</Text>
+      <TextInput
+        style={styles.text_input}
+        placeholder="Celular"
+        value={phone}
+        onChangeText={setPhone}
+        multiline={true}
+        numberOfLines={5}
+      />
+      <Text style={styles.text_label}>Fecha</Text>
+      <DatePicker
+        style={styles.content_date}
+        date={dateP}
+        locale="es"
+        mode = "date"
+        onDateChange={date =>setDateP(date)}
+        multiline={true}
+        numberOfLines={5}
+      ><DatePicker/>
       <Button title="Aceptar" onPress={newUser}></Button>
+      <Pressable
+        style={styles.btnExit} 
+        onPress={()=>setModalVisible(!modalVisible)}>Cancelar</Pressable>
     </Modal>
   );
 };
